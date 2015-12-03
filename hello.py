0dddf272b9caa14c32a2a06965c596383cfc1cb5
Sean Yang <comdaze@163.com>
2015-12-03 12:15:19 +0800

    modified hello.py file

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 import os
 import uuid
+import redis
+import json
+import urlparse
+import pwd
+import newrelic-admin
 from flask import Flask
 
 
@@ -9,10 +14,23 @@ BLUE = "#0099FF"
 GREEN = "#33CC33"
 
 COLOR = GREEN
-
+count = 0
 @app.route('/')
 def hello():
-
+    
+    global count
+    global value
+    count += 1
+    
+    rediscloud_service = json.loads(os.environ['VCAP_SERVICES'])['rediscloud'][0]
+    credentials = rediscloud_service['credentials']
+    r = redis.Redis(host=credentials['hostname'], port=credentials['port'], password=credentials['password'])
+    r.set('foo','bar')
+    value = r.get('foo')
+   
+   #newrelic_service = json.loads(os.environ['VCAP_SERVICES'])['newrelic'][0]
+   #credentials = newrelic_service['credentials']
+   #lic = credentials['licenseKey']
 
     return """
     <html>
@@ -20,13 +38,14 @@ def hello():
 
     <center><h1><font color="white">Hi, I'm GUID:<br/>
     {}</br>
-
+   <h2><font color="red">Count is: {}  
+   <h3><font color="pink">Redis get value is: {}  
 
     </center>
 
     </body>
     </html>
-    """.format(COLOR,my_uuid,)
+    """.format(COLOR,my_uuid,count,value,)
 
 if __name__ == "__main__":
 	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
